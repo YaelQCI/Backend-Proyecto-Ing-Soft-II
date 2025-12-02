685deb43e7a248b2f115a0b1a8d7ade555bd5932
iDinot21 <dani.ale.12@hotmail.es>
2025-12-02 10:31:21 -0600

    Add files via upload

--- back-osint/database/seeds/DatabaseSeeder.php
+++ back-osint/database/seeds/DatabaseSeeder.php
@@ -18,8 +18,12 @@ class DatabaseSeeder extends Seeder
             HerramientasSeeder::class,
             CasosSeeder::class,
             ChatbotsSeeder::class,
+            // ModuloCapturistaSeeder::class, // Descomentar para datos de prueba del modulo capturista
         ]);
         
-        $this->command->info('¡Todos los seeders ejecutados correctamente!');
+        $this->command->info('Todos los seeders ejecutados correctamente!');
+        $this->command->info('');
+        $this->command->info('Para datos de prueba del Modulo Capturista, ejecuta:');
+        $this->command->info('php artisan db:seed --class=ModuloCapturistaSeeder');
     }
 }
--- back-osint/routes/api.php
+++ back-osint/routes/api.php
@@ -21,3 +21,26 @@ Route::post('/verify', [App\Http\Controllers\Api\AuthController::class, 'verify'
 
 // Rutas protegidas
 Route::middleware('auth:api')->get('/user', [App\Http\Controllers\Api\UserController::class, 'show']);
+
+// Rutas del Módulo Capturista (protegidas)
+Route::middleware(['auth:api', 'capturista'])->prefix('capturista')->group(function () {
+    
+    // Gestión de casos asignados
+    Route::get('/casos', [App\Http\Controllers\Api\CapturistaController::class, 'getCasosAsignados']);
+    Route::get('/casos/{id}', [App\Http\Controllers\Api\CapturistaController::class, 'verCaso']);
+    
+    // Gestión de evidencias
+    Route::get('/casos/{idCaso}/evidencias', [App\Http\Controllers\Api\CapturistaController::class, 'getEvidencias']);
+    Route::post('/evidencias', [App\Http\Controllers\Api\CapturistaController::class, 'agregarEvidencia']);
+    Route::put('/evidencias/{id}', [App\Http\Controllers\Api\CapturistaController::class, 'actualizarEvidencia']);
+    Route::delete('/evidencias/{id}', [App\Http\Controllers\Api\CapturistaController::class, 'eliminarEvidencia']);
+    
+    // Generación de reportes
+    Route::get('/casos/{idCaso}/reporte-completo', [App\Http\Controllers\Api\CapturistaController::class, 'generarReporteCompleto']);
+    Route::get('/casos/{idCaso}/reporte-evidencias', [App\Http\Controllers\Api\CapturistaController::class, 'generarReporteEvidencias']);
+    Route::post('/casos/{idCaso}/reporte-personalizado', [App\Http\Controllers\Api\CapturistaController::class, 'generarReportePersonalizado']);
+    
+    // Gestión de reportes
+    Route::get('/casos/{idCaso}/reportes', [App\Http\Controllers\Api\CapturistaController::class, 'listarReportes']);
+    Route::get('/reportes/{nombreArchivo}/descargar', [App\Http\Controllers\Api\CapturistaController::class, 'descargarReporte']);
+});
