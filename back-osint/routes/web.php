aef04f59ed90f61fb97b23846bb57243784e4c7f
hazelmario6-hue <hazel.mario6@gmail.com>
2025-12-04 03:04:47 -0600

    Update web.php

--- back-osint/routes/web.php
+++ back-osint/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HerramientaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +26,10 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/herramientas', [HerramientaController::class, 'index']);
+    Route::get('/categorias', [HerramientaController::class, 'categorias']);
+    Route::post('/herramientas', [HerramientaController::class, 'store']);
+    Route::delete('/herramientas/{id}', [HerramientaController::class, 'destroy']);
 
     // Rutas de Reportes
     Route::get('/reports', [App\Http\Controllers\ReportsController::class, 'index'])->name('reports');
